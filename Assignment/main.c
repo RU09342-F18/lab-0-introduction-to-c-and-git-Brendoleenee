9ef03e335c7ab3cc352196312b4f4b547cb86426
Brendoleenee <35497853+Brendoleenee@users.noreply.github.com>
2018-09-10 16:34:20 -0400

    Update main.c

--- Assignment/main.c
+++ Assignment/main.c
@@ -3,7 +3,51 @@
 /**
  * main.c
  */
-int main(void)
+#include "math.h"
+int math(int num1, int num2, char Operator) //Pulled from the header used
 {
-	return 0;
+    if Operator == "+" //Addition Math Function
+    {
+         return (num1 + num2)
+    };
+    else if Operator == "-" //Subtraction Math Function
+    {
+         return (num1 - num2)
+    };
+    else if Operator == "*" //Multiplication Math Function
+        {
+         return (num1 * num2)
+        };
+    else if Operator == "/" //Division Math Function
+        {
+         return (num1 / num2)
+        };
+    else if Operator == "%" //Modulus Math Function
+        {
+         return (num1 % num2)
+        };
+    else if Operator == "<" //Left Shift Function
+        {
+         return (num1 << num2)
+        };
+    else if Operator == ">"//Right Shift Function
+        {
+         return (num1 >> num2)
+        };
+    else if Operator == "&"//Bitwise AND Function
+        {
+         return (num1 & num2)
+        };
+    else if Operator == "|"//Bitwise OR Function
+        {
+         return (num1 | num2)
+        };
+    else if Operator == "^"//Bitwise XOR Function
+        {
+         return (num1 ^ num2)
+        };
+    else if Operator == "~"//Bitwise Inverse Function
+            {
+         return (~num1)
+            };
 }
